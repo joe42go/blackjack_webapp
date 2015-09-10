c91f852bd0a49b7497126ea88d5f270585170981
Joe Chung <joe42go@gmail.com>
2015-09-10 22:33:54 +0000

    initial set up complete

--- main.rb
+++ main.rb
@@ -13,9 +13,15 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :secret => 'your_secret'
 
 get "/" do
-  "hello world"
+  erb :welcome
 end
 
-get "/test" do
-  redirect "/"
+get "/form" do
+  erb :form
+end
+
+post '/bet' do
+  @player_name = params[:player_name]
+  erb :bet
+  #:locals => {:player_name => params[:player_name]}
 end
